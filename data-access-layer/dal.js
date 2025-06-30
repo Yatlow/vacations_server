c8c0f44492b560458b4da1e61146f79782af4e9d
Yatlow <yisrael@atlow.co.il>
2025-06-30 18:16:56 +0300

    vacation_server

--- data-access-layer/dal.js
+++ data-access-layer/dal.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
+require('dotenv').config();
 
 const pool=new Pool({
-     connectionString: "postgres.bjafudzdwhkbxqeeueng:vacations@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
+     connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false 
   }
