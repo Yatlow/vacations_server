f4a06e64ffc3659a677ecab66cad333e7881391f
Yatlow <yisrael@atlow.co.il>
2025-06-30 18:21:00 +0300

    vacation_server

--- data-access-layer/dal.js
+++ data-access-layer/dal.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 require('dotenv').config();
-
+console.log("DB URL:", process.env.DATABASE_URL);
 const pool=new Pool({
      connectionString: process.env.DATABASE_URL,
   ssl: {
