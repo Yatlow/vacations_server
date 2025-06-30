2180c4b388514838b687973488646ae369e5fd4e
Yatlow <yisrael@atlow.co.il>
2025-06-30 20:49:04 +0300

    vacation_server

--- middleware/verify-admin.js
+++ middleware/verify-admin.js
@@ -1,6 +1,6 @@
 function verifyAdmin(request, response, next) {
-    console.log(request.user)
-    if (request.user && request.user.role=="admin")
+    console.log(request.headers.role)
+    if (request.headers.role=="admin")
         next();
     else {
         return response.status(401).send("Unauthorized (admin)");
