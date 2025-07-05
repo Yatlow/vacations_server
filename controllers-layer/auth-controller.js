42af1a0d05127aec0f487b977435141aab18ae77
Yatlow <yisrael@atlow.co.il>
2025-07-06 00:37:39 +0300

    updated

--- controllers-layer/auth-controller.js
+++ controllers-layer/auth-controller.js
@@ -58,16 +58,16 @@ router.post("/refresh", async (request, response) => {
         const { refreshToken } = request.body;
         if (!refreshToken) return response.status(400).send("Missing refresh token");
 
-        const decoded = jwt.verify(refreshToken, config.authSecrets.refreshSalt);
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_SALT);
         const [user] = await authLogic.getUserAsync([decoded.user.uuid]);
         if (!user) return response.status(401).send("User not found");
 
         const accessPayload = { user };
-        const token = jwt.sign(accessPayload, config.authSecrets.salt,
-            { expiresIn: config.server.tokenExpiration });
+        const token = jwt.sign(accessPayload,  process.env.AUTH_SALT,
+            { expiresIn: process.env.REFRESH_EXP });
         const newRefresh = jwt.sign({ user: { uuid: user.uuid }},
-            config.authSecrets.refreshSalt,
-            { expiresIn: config.server.refreshExpiration });
+            process.env.REFRESH_SALT,
+            { expiresIn: process.env.REFRESH_EXP });
         return response.send({ token, refreshToken: newRefresh });
     }
     catch (error) {
