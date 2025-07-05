3dc8792bacfe7a26ab2f10173f350ac1d87160ab
Yatlow <yisrael@atlow.co.il>
2025-07-06 01:15:24 +0300

    updated

--- controllers-layer/auth-controller.js
+++ controllers-layer/auth-controller.js
@@ -59,6 +59,7 @@ router.post("/refresh", async (request, response) => {
         if (!refreshToken) return response.status(400).send("Missing refresh token");
 
         const decoded = jwt.verify(refreshToken, process.env.REFRESH_SALT);
+        console.log(decoded)
         const res= await authLogic.getUserAsync(decoded.user.uuid);
         console.log(res);
         const user= (res.rows[0])
